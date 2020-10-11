b89a8ec7587e4002d78267111128a3bb59e001fa
acuscorp <acuscorp@gmail.com>
2020-10-10 20:01:11 -0500

    modificaciones echas para hacer responsive la vista principal

--- APP/pages/index.js
+++ APP/pages/index.js
@@ -3,9 +3,9 @@ import { Nav } from "../components/nav/Nav";
 import { Footer } from "../components/footer/footer";
 
 const Landing = () => {
-    return (
-        <div>
-        <style jsx>{`
+  return (
+    <div>
+      <style jsx>{`
                 *{
                     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
                   
@@ -15,24 +15,19 @@ const Landing = () => {
                     background-position: center;
                     background-repeat: no-repeat;
                     background-size: cover;
-                    position: relative;
+                    display:flex;
                     height: 90vh;
                   }
-                  
-                  .landInfo{
-                    position: absolute;
-                    left: 20%;
-                    z-index: 2;
+                  .landigContainer{
+                    display: flex;
+                  }
+                  .imgLandInfo{
                     margin-top: 8%;
+                  }
+                  .landInfo{
+                    padding-top: 15%;
                     font-size: 50px;
                     color: #342c2c;
-                  }
-                  
-                  .imgLandInfo{
-                    position: absolute;
-                    left: 5%;
-                    z-index: 2;
-                    margin-top: 11%;
                   }
                   
                   .sections-center{
@@ -40,7 +35,6 @@ const Landing = () => {
                     font-size: 30px;
                     margin-top: 40px;
                   }
-                  
                   .sections-imgInfo{
                     display: flex;  
                     justify-content: center;
@@ -49,39 +43,72 @@ const Landing = () => {
                     font-size: 20px;
                     margin-top: 40px;
                   }
+
+                  @media  (max-width: 769px) {
+                    .landInfo{ 
+                      left: 35%;
+                      font-size: 40px;
+                      margin-top: 15%;
+                    }
+                    .imgLandInfo{
+                      margin-top: 25%;
+                    }
+                    #imgLandingInfo {
+                      max-width: 70%;
+                    }
+                    .sections-imgInfo{
+                      flex-direction: column;
+                    }
+                  }
+
+                  @media (max-width: 426px) {
+                    .landingContainer{
+                      flex-direction: column;
+                    }
+                    .imgLandInfo{
+                      margin-top: 60%;
+                      max-width: 30%;
+                    }
+                    .landInfo{
+                      button: 20%;
+                      font-size: 26px;
+                      margin-top: 60%;
+                    }
+                  }
         `}</style>
-        <Nav/>
-        <div className="landing">
-            <div className="imgLandInfo">
-                <img src='images/imgLandInfo.png' alt="movil" width="200px" />
-            </div>
-            <div className="landInfo">
-                <p>LO MEJOR PARA <br />PACIENTES</p>
-                <p>LO MEJOR PARA <br />ENFERMERA(O)S</p>
-            </div>
+      <Nav />
+      <div className="landing">
+        <div className="landigContainer">
+          <div className="imgLandInfo">
+            <img src='images/imgLandInfo.png' alt="movil" id="imgLandingInfo" max-width="50%" />
+          </div>
+          <div className="landInfo" max-width="200px">
+            <p>LO MEJOR PARA <br />PACIENTES</p>
+            <p>LO MEJOR PARA <br />ENFERMERA(O)S</p>
+          </div>
         </div>
-        <section className="sections-center">
-                Somos una compañia formada por profesionales<br/> de la salud y tecnología que constantemente estamos buscando<br/> maneras de improvisar el sistema de salud.
+      </div>
+      <section className="sections-center">
+        Somos una compañia formada por profesionales<br /> de la salud y tecnología que constantemente estamos buscando<br /> maneras de improvisar el sistema de salud.
          </section>
-         <section className="sections-imgInfo">
-                <p>
-                    La plataforma Nurse Go es la solucion <br/> para la creciente demanda del sistema de salud.<br/><br/>
-                    La selección de enfermera(o)s es un desafío, ya que se debe investigar<br/> al personal de salud que mas accesible sea para nosotros y eso es algo que consume<br/> tiempo. Nuestra plataforma ayuda a que el proceso de contratación de un servicio <br/> médico sea mas rápido y con los costos que se adapten a ti.
+      <section className="sections-imgInfo">
+        <p>
+          La plataforma Nurse Go es la solucion <br /> para la creciente demanda del sistema de salud.<br /><br />
+                    La selección de enfermera(o)s es un desafío, ya que se debe investigar<br /> al personal de salud que mas accesible sea para nosotros y eso es algo que consume<br /> tiempo. Nuestra plataforma ayuda a que el proceso de contratación de un servicio <br /> médico sea mas rápido y con los costos que se adapten a ti.
             </p>
-                <img src="images/section1Landing.png" alt="" height="250px" />
-            </section>
+        <img src="images/section1Landing.png" alt="" max-height="250px" />
+      </section>
 
-            <section className="sections-imgInfo">
-                <img src="images/section2Landing.png" alt="" height="250px" />
-                <p>
-                    Contratar un servicio de enfermeria nunca había sido tan fácil!<br /> <br />
-                    Estamos encontrando mejores maneras para que nuestros profesionistas<br /> maximicen la experiencia del paciente. La plataforma<br />  Nurse Go apunta a reducir el tiempo de contratación de un servicio de enfermería<br />  y tener un catalogo de precios que pueda favorecer al paciente.
-             </p>
-            </section>
-            <Footer/>
-        </div>
+      <section className="sections-imgInfo">
+        <img src="images/section2Landing.png" alt="" max-height="250px" />
+        <p>
+          Contratar un servicio de enfermeria nunca había sido tan fácil!<br /> <br />
+                Estamos encontrando mejores maneras para que nuestros profesionistas<br /> maximicen la experiencia del paciente. La plataforma<br />  Nurse Go apunta a reducir el tiempo de contratación de un servicio de enfermería<br />  y tener un catalogo de precios que pueda favorecer al paciente.
+              </p>
+      </section>
+      <Footer />
+    </div>
 
-    )
+  )
 }
-
 export default Landing;
